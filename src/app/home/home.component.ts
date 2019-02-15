5165a9661cad2a32fddea186726fbeccc1061657
Gagan Daroach <gagandaroach@gmail.com>
2019-02-14 20:53:35 -0600

    updated main screen a little, added some titles and form generation attempts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,17 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import CustomValidators from '../forms/CustomValidators';
 
 @Component({
   selector: 'home',
   styleUrls: ['./home.component.css'],
   templateUrl: './home.component.html'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+    homeForm: FormGroup;
+    constructor(private formBuilder: FormBuilder) {}
+
+    ngOnInit() {
+      
+    }
 }
